491289b594a324698190eb4a87770f2e55bf38fe
snorkrat <snorkrat@gmail.com>
2023-01-24 19:01:38 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 PUID=${PUID:-911}
 PGID=${PGID:-911}
+INSTALL_DIR=${INSTALL_DIR:-0}
 
 groupmod -o -g "$PGID" abc
 usermod -o -u "$PUID" abc
@@ -14,6 +15,17 @@ User gid:    $(id -g abc)
 chown abc:abc /var/www/html
 chown abc:abc /hesk
 
+if [[ $INSTALL_DIR -eq 0 ]]; then
+echo "Removing Install DIR before copy"
+rm -rf /hesk/install
+fi
+
 mv -n /hesk/* /var/www/html/
+
+rm -rf /hesk
+
+echo "Removing Install DIR from web root"
+rm -rf /var/www/html/install
+
 
 exec tail -f /dev/null
